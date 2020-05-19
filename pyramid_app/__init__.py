006d7bd1970424f122f81e5da340387f51365c83
Nabil Khoury <nabilgkhoury@hotmail.com>
2020-05-18 23:32:58 -0400

    fixed issue whereby static files like styles.css weren't being served; moved elasticsearch connection constants to module __init__

--- pyramid_app/__init__.py
+++ pyramid_app/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 from pyramid.config import Configurator
 
+# es connection
+ES_CONNECT = (dict(host='es-service', port=9200),)
+ES_INDEX = 'crunshbase'
+
 
 def main():
     config = Configurator()
@@ -11,6 +15,7 @@ def main():
     config.add_route('login', '/login')
     config.add_route('search', '/search')
     config.add_route('company', '/company')
+    config.add_static_view(name='static', path='pyramid_app:static')
 
     # scan views
     config.scan('.views')
