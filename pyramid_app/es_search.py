b01f4c8b78df3e9d6b4bfc8bf11e7e86f3955348
Nabil Khoury <nabilgkhoury@hotmail.com>
2020-05-19 07:53:44 -0400

    Implemented first version of elasticsearch convenience wrapper: phrase search by company name, company location, company id and full-text search on all fields

--- pyramid_app/es_search.py
+++ pyramid_app/es_search.py
@@ -7,19 +7,68 @@ from pyramid_app import ES_INDEX, ES_CONNECT
 
 class ESSearch(object):
     es_client: Elasticsearch
+    es_index: str
 
-    def __init__(self, connect=ES_CONNECT):
-        self.es_client = Elasticsearch(ES_CONNECT)
+    def __init__(self, connect=ES_CONNECT, index=ES_INDEX):
+        self.es_client = Elasticsearch(connect)
+        self.es_index = index
 
     def search_by_name(self, search: str):
-        raise NotImplementedError
+        res = self.es_client.search(
+            index=self.es_index,
+            doc_type='company',
+            body=dict(
+                query=dict(
+                    match_phrase=dict(
+                        company_name=search
+                    )
+                )
+            )
+        )
+        return [company['_source'] for company in res['hits']['hits']]
 
     def search_by_location(self, search: str):
-        raise NotImplementedError
+        res = self.es_client.search(
+            index=self.es_index,
+            doc_type='company',
+            body=dict(
+                query=dict(
+                    match_phrase=dict(
+                        location=search
+                    )
+                )
+            )
+        )
+        return [company['_source'] for company in res['hits']['hits']]
 
-    def search_by_event(self, search: str):
-        raise NotImplementedError
+    def search_by_id(self, company_id: str):
+        res = self.es_client.search(
+            index=self.es_index,
+            doc_type='company',
+            body=dict(
+                query=dict(
+                    match=dict(
+                        company_id=company_id
+                    )
+                )
+            )
+        )
+        if len(res['hits']['hits']):
+            return res['hits']['hits'][0]['_source']
+        else:
+            return {}
 
     def search_by_text(self, text: str):
-        raise NotImplementedError
+        res = self.es_client.search(
+            index=self.es_index,
+            doc_type='company',
+            body=dict(
+                query=dict(
+                    query_string=dict(
+                        query=text
+                    )
+                )
+            )
+        )
+        return [company['_source'] for company in res['hits']['hits']]
 
