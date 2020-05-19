dbce8a6bdc2a9f69474f111769362b58659944ce
Nabil Khoury <nabilgkhoury@hotmail.com>
2020-05-19 07:56:54 -0400

    First useful implementation of search and company views to go with the new templates; Homepage now redirects automatically to search page

--- pyramid_app/views.py
+++ pyramid_app/views.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 from pyramid.response import Response
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPNotFound, HTTPFound
+
+from pyramid_app.es_search import ESSearch
 
 
 @view_config(route_name='hello')
 def hello_world(request):
-    print('Request inbound!')
-    return Response('Docker works with Pyramid!')
+    return HTTPFound(location='/search')
 
 
 @view_config(route_name='login', renderer='templates/login.jinja2')
@@ -16,10 +18,21 @@ def login(request):
 
 @view_config(route_name='search', renderer='templates/search.jinja2')
 def search_companies(request):
-    return dict(title='Search Page')
+    text = request.params.get('search_field', '')
+    hits = []
+    if text:
+        es_search = ESSearch()
+        hits = es_search.search_by_text(text=text)
+    return dict(title='Search Page', text=text, hits=hits)
 
 
 @view_config(route_name='company', renderer='templates/company.jinja2')
 def company_profile(request):
-    return dict(title='Company Profile')
+    company_id = request.params.get('company_id', '')
+    if company_id:
+        es_search = ESSearch()
+        company_doc = es_search.search_by_id(company_id=company_id)
+        return dict(title='Company Profile', company=company_doc)
+    else:
+        raise HTTPNotFound()
 
